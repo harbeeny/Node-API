342d8605acb8c1acc8e5a84663bd5fdf671f64d3
Hunter Arbeeny <144467896+harbeeny@users.noreply.github.com>
2024-04-12 09:45:33 -0400

    Put and Delete endpoints added

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const Product = require('./models/product.model')
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 
 app.get('/', (req, res) => {
     res.send("Hello from NODE API Server");
@@ -35,6 +36,41 @@ app.get('/api/product/:id', async (req, res) => {
         const { id } = req.params;
         const product = await Product.findById(id);
         res.status(200).json(product);
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+})
+
+app.put('/api/product/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const product = await Product.findByIdAndUpdate(id, req.body);
+
+        if(!product) {
+            return res.status(404).json({message: "Product not found"});
+        }
+        
+        const updatedProduct = await Product.findById(id);
+        res.status(200).json(updatedProduct);
+        
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+})
+
+app.delete('/api/product/:id', async (req, res) => {
+    try {
+        const {id} = req.params;
+        
+        const product = await Product.findByIdAndDelete(id);
+        
+        if(!product) {
+            return res.status(404).json({message: "Product not found"});
+        }
+
+        res.status(200).json({message: "Product deleted successfully"});
+
     } catch (error) {
         res.status(500).json({message: error.message});
     }
